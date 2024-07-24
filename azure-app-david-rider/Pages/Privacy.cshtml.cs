ec8bfab7ad86195b1c557dd99b2d08c10437be0a
davidscheidtbitstop <123389048+davidscheidtbitstop@users.noreply.github.com>
2024-07-24 15:00:07 -0400

    Initial commit

---
+++ azure-app-david-rider/Pages/Privacy.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_david_rider.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
